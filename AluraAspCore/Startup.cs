9fded481026d6361ccc55fc9f5f389ccffe47d1d
Wesley Trindade <wesley.trindade.santos@gmail.com>
2018-04-09 17:00:44 -0300

    new Commit

--- AluraAspCore/Startup.cs
+++ AluraAspCore/Startup.cs
@@ -43,7 +43,7 @@ namespace AluraAspCore
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Pedido}/{action=Carrossel}/{id?}");
             });
         }
     }
